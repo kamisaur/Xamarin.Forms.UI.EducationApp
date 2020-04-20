80cbe0106d19a0ab6100536228b0f924188a7b59
Kamil <kamil@boostorder.com>
2020-04-20 20:51:57 +0800

    TimeSpan converter added

---
+++ source/EducationApp/Converters/TimeSpanConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EducationApp.Converters
+{
+    public class TimeSpanConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var time = (TimeSpan)value;
+            var output = "";
+
+            if(time.Hours != 0)
+            {
+                output = $"{time.Hours} h";
+            }
+
+            if(time.Minutes != 0)
+            {
+                output = $"{output} {time.Minutes} min";
+            }
+
+
+            return output;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+    }
+}
