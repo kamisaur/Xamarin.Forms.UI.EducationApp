058fdded08b05d6ea9a6400717e6584081e11444
Kamil <kamil@boostorder.com>
2020-05-25 12:01:52 +0800

    Label tabs bug fixed; Detailed Course title label fixed

---
+++ source/EducationApp/Converters/BoolToTabColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EducationApp.Converters
+{
+    public class BoolToTabColorConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isSelected = (bool)value;
+            if (isSelected)
+            {
+                return (Color)Application.Current.Resources["PrimaryText"];
+            }
+            else
+            {
+                return (Color)Application.Current.Resources["TabUnselected"];
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
